73d1cb346faed859ccc8494acdeba31578b709e5
Mahad Hussaini <mahadarshad214@gmail.com>
2021-06-30 04:31:08 +0500

    Create counter context and counter reducer sample

---
+++ src/Child.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import counterContext from "./CounterContext";
+
+const Child = () => {
+  let counterValue = useContext(counterContext);
+  console.log(counterContext);
+  return (
+    <div>
+      <h2>This is first child using Counter Context</h2>
+      <h3>Counter value is: {counterValue[0]}</h3>
+
+      <button
+        onClick={() => {
+          counterValue[1](++counterValue[0]);
+        }}
+      >
+        Increment Context
+      </button>
+    </div>
+  );
+};
+
+export default Child;
